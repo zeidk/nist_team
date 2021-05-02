8416cb09c06baf6b2ee0d64bed255521bc0ad7aa
Zeid Kootbally <zeid.kootbally@nist.gov>
2021-05-02 14:17:01 -0400

    Update gantry_control.h

--- include/gantry_control.h
+++ include/gantry_control.h
@@ -14,9 +14,6 @@
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
-#include <moveit_msgs/AttachedCollisionObject.h>
-#include <moveit_msgs/CollisionObject.h>
-#include <moveit_visual_tools/moveit_visual_tools.h>
 
 
 #include <boost/foreach.hpp>
